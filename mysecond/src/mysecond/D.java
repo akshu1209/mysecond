a3aa2bc6853b67ff073802a254bca1fa24ada34f
Admin <Admin@Admin-PC>
2021-07-17 01:56:01 -0700

    okay

---
+++ mysecond/src/mysecond/D.java
@@ -0,0 +1,25 @@
+package mysecond;
+
+public class D {
+	
+	
+   public void Akshay()
+   
+   {
+	   final long b=12334,c=329849,d=478849;
+	   System.out.println(b+c+d);
+   }
+   
+   
+   public static void main (String []args)
+   {
+	   
+	   D c=new D();
+	   c.Akshay();
+	   F s=new F();
+	   s.m1();
+	   
+   }
+}
+
+
---
+++ mysecond/src/mysecond/F.java
@@ -0,0 +1,19 @@
+package mysecond;
+
+public class F
+{
+	
+	public void m1()
+	{
+		
+		char b= 67 ;               // 67=C ASC   
+		System.out.println(b);               // h=124 is as asc american std code interchange
+		
+	}
+	
+	
+}
+
+
+
+
